6eeeb4ef40d4f65a68aae191bd7bab967015f236
Neydacha9 <slavaezdin9@gmail.com>
2025-10-15 12:29:36 +0500

    Add files via upload

---
+++ num1, num2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp4
+{
+    public class Car
+    {
+        public void Refonly(ref int num1, ref int num2)
+        {
+            num1++;
+            num2++;
+        }
+        public void Refout(ref int num1, ref int result)
+        {
+            num1++;
+            result = num1;
+        }
+
+    }
+}
