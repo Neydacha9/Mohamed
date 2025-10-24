e05ef7286538e7509f92039d4590b009a6898797
Neydacha9 <slavaezdin9@gmail.com>
2025-10-24 15:57:15 +0500

    Add files via upload

---
+++ ConsoleApp6/calc1/calc1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc1
+{
+    internal interface ICalc1
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public int num3 { get; set; }
+        public int addition(int num1, int num2);
+        public int substraction(int num1, int num2);
+        public int division(int num1, int num2);
+        public int multiplication(int num1, int num2);
+    }
+}
